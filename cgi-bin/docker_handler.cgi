d3474f6f65f507aa302b794340a6b199a8da4483
root <root@d1stkfactory>
2015-04-10 15:17:28 +0000

    first backup

---
+++ cgi-bin/docker_handler.cgi
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+if [ "$REQUEST_METHOD" = "POST" ]; then
+  POST=$(</dev/stdin)
+  # Start / Stop container
+  if [[ $POST =~ ^toggleService=(.*)$ ]]; then
+    service="${BASH_REMATCH[1]}"
+    if [[ "$(docker ps)" =~ $service ]]; then
+      docker stop $service > /dev/null
+    else
+      docker start $service > /dev/null
+    fi
+  fi
+  # Rename container
+  if [[ $POST =~ ^newName=(.*)\&oldName=(.*)$ ]]; then
+    newName="${BASH_REMATCH[1]}"
+    oldName="${BASH_REMATCH[2]}"
+    docker rename "$oldName" "$newName"
+  fi
+fi
+
+echo "Content-type: text/html"
+echo ""
+
+echo '<html>'
+echo '<head>'
+echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">'
+echo '<title>Adminsys</title>'
+echo '<link rel="stylesheet" type="text/css" href="../style/style.css">'
+echo '<link rel="stylesheet" type="text/css" href="../style/togglebutton.css">'
+echo '</head>'
+echo '<body>'
+echo '  <div id="header">'
+echo '  </div>'
+echo '  <div id="content">'
+echo '    <div id="userinfo">'
+echo '      <p>Nom : root</p>'
+echo '      <p>Pass : root</p>'
+echo '    </div>'
+echo '    <div id="services">'
+python ../format/containers.py "$(docker ps -a)"
+echo '      <div id="tools">'
+echo '        <input type="button" onclick="location.replace(location.href)" value="Reload" id="reloadButton">'
+echo '      </div>'
+echo '    </div>'
+echo '    <div id="installer">'
+echo '      <form method="post" action="docker-handler.cgi">'
+echo '        <input type="checkbox" value="Wordpress" id="wordpress">'
+echo '        <label for="wordpress"><img src="https://s.w.org/favicon.ico" width="15" height="15">Wordpress</label>'
+echo '        <div class="installerMenu" id="wordpressInstaller">'
+echo '          some stuff here to config'
+echo '        </div>'
+echo '        <input type="button" value="Créer les services" style="display:block; margin:20 0 0 10px;">'
+echo '      </form>'
+echo '    </div>'
+echo '  </div>'
+echo '</body>'
+echo '</html>'
+
+exit 0
---
+++ format/containers.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+#-*-coding:utf8-*
+
+import sys
+
+class Parser(object):
+  def __init__(self, string):
+    self.string = string.split('\n')
+    self.columnName = ["CONTAINER ID", "IMAGE", "COMMAND", "CREATED", "STATUS", "PORTS", "NAMES"]
+    self.indexes = [self.string[0].index(columnName) for columnName in self.columnName]
+
+  def parse(self):
+    self.data = []
+    for row in self.string[1:]:
+      r = []
+      for i in range(len(self.columnName)-1):
+        r.append(row[self.indexes[i]:self.indexes[i+1]].strip())
+      r.append(row[self.indexes[-1]:].strip())
+      self.data.append(r)
+
+  def format(self):
+    print('<table>')
+    print('<tr>')
+    for c in self.columnName:
+      print('<th>{0}</th>'.format(c))
+    print('<th>TOGGLE</th>')
+    print('</tr>')
+    for row in self.data:
+      print('<tr>')
+      for cellContent in row[:-1]:
+        print('<td>{0}</td>'.format(cellContent))
+      print('<td>')
+      print('<form method="post" action="docker_handler.cgi">')
+      print('<input type="text" name="newName" value="{0}" class="containerName" onfocus="this.style.border=\'1px solid lightgrey\'; this.style.background=\'white\'" onblur="this.style.border=\'none\'; this.style.background=\'transparent\'; if (this.value!=this.form.oldName.value) this.form.submit()" >'.format(row[-1]))
+      print('<input type="hidden" name="oldName" value="{0}">'.format(row[-1]))
+      print('</form>')
+      print('</td>')
+      print('<td>')
+      print('<form method="post" action="docker_handler.cgi">')
+      print('<label class="switch">')
+      print('<input type="checkbox" onclick="this.form.submit();" class="switch-input" {0}>'.format("checked" if "Up" in row[4] else ""))
+      print('<input name="toggleService" type="hidden" value="{0}">'.format(row[0]))
+      print('<span data-off="Off" data-on="On" class="switch-label"></span>')
+      print('<span class="switch-handle"></span>')
+      print('</label>')
+      print('</form>')
+      print('</td>')
+      print('</tr>')
+    print('</table>')
+
+def main():
+  p = Parser(sys.argv[1])
+  p.parse()
+  p.format()
+
+if __name__=="__main__":
+  main()
