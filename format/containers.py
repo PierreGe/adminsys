d46df2caa3e0b23f48af30119054f26372dea7c2
Maribib <maribib.cerkinfo@gmail.com>
2015-04-11 15:20:55 +0200

    Update containers.py

--- format/containers.py
+++ format/containers.py
@@ -17,6 +17,15 @@ class Parser(object):
         r.append(row[self.indexes[i]:self.indexes[i+1]].strip())
       r.append(row[self.indexes[-1]:].strip())
       self.data.append(r)
+      
+  def getExtenalPort(self,string):
+    i = sting.find("->")
+    if i==-1:
+      return None
+    j=i-1
+    while string[j]!=':':
+      j-=1
+    return string[j:i]
 
   def format(self):
     print('<table>')
@@ -29,35 +38,41 @@ class Parser(object):
     print('</tr>')
     for row in self.data:
       print('<tr>')
-      for cellContent in row[:-1]:
+      port = self.getExternalPort(row[-2])
+      if (port!=None):
+        print('<td><a href="http://supayr.ninja:{1}">{0}</a></td>'.format(row[0],port))
+      else:
+        print('<td>{0}</td>'.format(row[0]))
+        
+      for cellContent in row[1:-1]:
         print('<td>{0}</td>'.format(cellContent))
       print('<td>')
-      print('<form method="post" action="docker_handler.cgi">')
-      print('<input type="text" name="newName" value="{0}" class="containerName" onfocus="this.style.border=\'1px solid lightgrey\'; this.style.background=\'white\'" onblur="this.style.border=\'none\'; this.style.background=\'transparent\'; if (this.value!=this.form.oldName.value) this.form.submit()" >'.format(row[-1]))
-      print('<input type="hidden" name="oldName" value="{0}">'.format(row[-1]))
-      print('</form>')
+      print('  <form method="post" action="docker_handler.cgi">')
+      print('    <input type="text" name="newName" value="{0}" class="containerName" onfocus="this.style.border=\'1px solid lightgrey\'; this.style.background=\'white\'" onblur="this.style.border=\'none\'; this.style.background=\'transparent\'; if (this.value!=this.form.oldName.value) this.form.submit()" >'.format(row[-1]))
+      print('    <input type="hidden" name="oldName" value="{0}">'.format(row[-1]))
+      print('  </form>')
       print('</td>')
       print('<td>')
-      print('<form method="post" action="docker_handler.cgi">')
-      print('<label class="switch">')
-      print('<input type="checkbox" onclick="this.form.submit();" class="switch-input" {0}>'.format("checked" if "Up" in row[4] else ""))
-      print('<input name="toggleService" type="hidden" value="{0}">'.format(row[0]))
-      print('<span data-off="Off" data-on="On" class="switch-label"></span>')
-      print('<span class="switch-handle"></span>')
-      print('</label>')
-      print('</form>')
+      print('  <form method="post" action="docker_handler.cgi">')
+      print('    <label class="switch">')
+      print('      <input type="checkbox" onclick="this.form.submit();" class="switch-input" {0}>'.format("checked" if "Up" in row[4] else ""))
+      print('      <input name="toggleService" type="hidden" value="{0}">'.format(row[0]))
+      print('      <span data-off="Off" data-on="On" class="switch-label"></span>')
+      print('      <span class="switch-handle"></span>')
+      print('    </label>')
+      print('  </form>')
       print('</td>')
       print('<td>')
-      print('<form method="post" action="docker_handler.cgi">')
-      print('<input type="button" onclick="this.form.submit();" value="Duplicate/Fork" id="duplicate-button-{0}">'.format(row[0]))
-      print('<input name="duplicate" type="hidden" value="{0}">'.format(row[0]))
-      print('</form>')
+      print('  <form method="post" action="docker_handler.cgi">')
+      print('    <input type="button" onclick="this.form.submit();" value="Duplicate/Fork" id="duplicate-button-{0}">'.format(row[0]))
+      print('    <input name="duplicate" type="hidden" value="{0}">'.format(row[0]))
+      print('  </form>')
       print('</td>')
       print('<td>')
-      print('<form method="post" action="docker_handler.cgi">')
-      print('<input type="button" onclick="this.form.submit();" value="Delete" id="delete-button-{0}">'.format(row[0]))
-      print('<input name="delete" type="hidden" value="{0}">'.format(row[0]))
-      print('</form>')
+      print('  <form method="post" action="docker_handler.cgi">')
+      print('    <input type="button" onclick="this.form.submit();" value="Delete" id="delete-button-{0}">'.format(row[0]))
+      print('    <input name="delete" type="hidden" value="{0}">'.format(row[0]))
+      print('  </form>')
       print('</td>')
       
       print('</tr>')
