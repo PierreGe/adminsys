d8281bc39e1a603097d75d7d14c022465f68c659
Maribib <maribib.cerkinfo@gmail.com>
2015-04-11 15:36:33 +0200

    Update containers.py

--- format/containers.py
+++ format/containers.py
@@ -40,7 +40,7 @@ class Parser(object):
       print('<tr>')
       port = self.getExternalPort(row[-2])
       if (port!=None):
-        print('<td><a href="http://supayr.ninja:{1}">{0}</a></td>'.format(row[0],port))
+        print('<td><a href="http://supayr.ninja:{1}" target="_blank">{0}</a></td>'.format(row[0],port))
       else:
         print('<td>{0}</td>'.format(row[0]))
         
