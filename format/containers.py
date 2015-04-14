2f4a71717611247871c8a96b3e3d555895d78583
Maribib <maribib.cerkinfo@gmail.com>
2015-04-14 15:05:00 +0200

    Update containers.py

--- format/containers.py
+++ format/containers.py
@@ -25,7 +25,10 @@ class Parser(object):
     j=i-1
     while string[j]!=':':
       j-=1
-    return (string[:j],string[j+1:i])
+    k=i+2
+    while string[k]!='/':
+      k+=1
+    return (string[i+2:k],string[j+1:i])
 
   def format(self):
     print('<table>')
